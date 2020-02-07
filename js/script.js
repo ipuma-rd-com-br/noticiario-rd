0583fbc65fc9eda5679acb0f00501106fe7f22e5
Treinamento <Treinamento@RD723902.raiadrogasil.com>
2020-02-07 17:38:41 -0300

    commit 1a versao

--- js/script.js
+++ js/script.js
@@ -1,51 +1,49 @@
 /*
-Chamadas AJAX no JS
+Chamadas AJAX no JS com fetch() e .then()
 */
 
 const API_KEY = 'bfcfceca4ac94ee3807f66661b4d9c00';
 
-let config = {
-    method:'GET',
-    body:null // usado quando method='POST'
-}
+let btnCarregarNoticias = document.getElementById("btnCarregarNoticias");
 
-let response = fetch('https://newsapi.org/v2/top-headlines?country=br&apiKey='+API_KEY, config) // params: action, config
-                .then(
-                    // param: um callback
-                    (response)=>{
-                        return response.json();
-                    }
-                ) 
-                .then(
-                    // aqui vai o que tem que acontecer com a resposta do fetch...
-                    (json)=>{
-                        mostrarNaTela(json.articles);
-                    }
-                )
-                ; 
+btnCarregarNoticias.onclick = ()=>{
+        let config = {
+            method:'GET',
+            body:null // usado quando method='POST'
+        }
+        let response = fetch('https://newsapi.org/v2/top-headlines?country=br&apiKey='+API_KEY, config) // params: action, config
+                        .then(
+                            // param: um callback
+                            (response)=>{
+                                return response.json();
+                            }
+                        ) 
+                        .then(
+                            // aqui vai o que tem que acontecer com a resposta do fetch...
+                            (json)=>{
+                                mostrarNaTela(json.articles);
+                            }
+                        ); 
+    }
+
 
 /*
  * Exibe as noticias na tela HTML
  */
 function mostrarNaTela(noticias) {
-
     let divNoticias = document.getElementById("divNoticias");
-
-    noticias.forEach((noticia) => {
-
-        let htmlCardNoticia = 
-            `<div class="card col-md-4">
-                <img class="card-img-top" src="${noticia.urlToImage}" alt="${noticia.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${noticia.title}</h5>
-                    <p class="card-text text-justify">${noticia.description}</p>
-                    <a href="${noticia.url}" class="btn btn-primary">Ir para site</a>
-                </div>
-            </div>`;
-        
-        divNoticias.insertAdjacentHTML('beforeend', htmlCardNoticia);
-            
-    });
-
-    
+    noticias.forEach(
+        (noticia) => {
+            let htmlCardNoticia = 
+                `<div class="card col-md-4">
+                    <img class="card-img-top" src="${noticia.urlToImage}" alt="${noticia.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${noticia.title}</h5>
+                        <p class="card-text text-justify">${noticia.description}</p>
+                        <a href="${noticia.url}" class="btn btn-primary">Ir para site</a>
+                    </div>
+                </div>`;
+            divNoticias.insertAdjacentHTML('beforeend', htmlCardNoticia);
+        }
+    );
 }
